4a4ecb44ba905efaf5c1c4d32a890f038aa0685f
mrbudbud <rachmad.16421044@student.ubl.ac.id>
2021-12-08 18:13:33 +0700

    new logo

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,7 +15,7 @@
           Quasar App
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>Comingsoon new website</div>
       </q-toolbar>
     </q-header>
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,10 @@
 <template>
+
   <q-page class="flex flex-center">
     <img
       alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+      src="~assets/logo.png"
+      style="width: 200px; height: 150px"
     >
   </q-page>
 </template>
